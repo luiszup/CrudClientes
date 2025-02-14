bd4363b637c0be43d6a55bcab42405c68ea13958
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-14 15:00:15 -0300

    feat: add ClientService.java file with attribute ClientRepository and constructor

--- src/main/java/com/crud/crudClientes/service/ClientService.java
+++ src/main/java/com/crud/crudClientes/service/ClientService.java
@@ -1,4 +1,15 @@
 package com.crud.crudClientes.service;
 
+import com.crud.crudClientes.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
 }
