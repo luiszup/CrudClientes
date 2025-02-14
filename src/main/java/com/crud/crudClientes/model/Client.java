2ad7763651b6582c1806c20c63e98ed5d55dad41
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-14 14:37:02 -0300

    feat: add Client entity in model

---
+++ src/main/java/com/crud/crudClientes/model/Client.java
@@ -0,0 +1,19 @@
+package com.crud.crudClientes.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private String email;
+
+    @Column(name = "telefone")
+    private String phone;
+}
