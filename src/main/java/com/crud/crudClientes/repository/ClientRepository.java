201b824e04a5f90a17a3b40bf655d6c203cd8e23
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-14 14:53:35 -0300

    feat: add ClientRepository.java file

---
+++ src/main/java/com/crud/crudClientes/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.crud.crudClientes.repository;
+
+import com.crud.crudClientes.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
