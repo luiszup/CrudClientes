7d8820fe83b603fd90df13cb262f51ecd29f194d
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-14 15:22:11 -0300

    feat: add ClientController.java file with ClientService attribute and constructor

---
+++ src/main/java/com/crud/crudClientes/controller/ClientController.java
@@ -0,0 +1,15 @@
+package com.crud.crudClientes.controller;
+
+import com.crud.crudClientes.service.ClientService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+}
