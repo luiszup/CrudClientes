32d065a1247073ec758bf0e7627f642796f3111e
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-14 14:58:35 -0300

    fix: add @Repository annotation in ClientRepository interface

--- src/main/java/com/crud/crudClientes/repository/ClientRepository.java
+++ src/main/java/com/crud/crudClientes/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.crud.crudClientes.repository;
 
 import com.crud.crudClientes.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
---
+++ src/main/java/com/crud/crudClientes/service/ClientService.java
@@ -0,0 +1,4 @@
+package com.crud.crudClientes.service;
+
+public class ClientService {
+}
