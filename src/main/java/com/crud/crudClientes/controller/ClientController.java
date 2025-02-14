521cde2f9f468024f97c9ded455baf8a46925937
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-14 15:45:53 -0300

    feat: add GetMapping and add getAllClients method in ClientController

--- src/main/java/com/crud/crudClientes/controller/ClientController.java
+++ src/main/java/com/crud/crudClientes/controller/ClientController.java
@@ -1,8 +1,12 @@
 package com.crud.crudClientes.controller;
 
+import com.crud.crudClientes.dto.ClientDTO;
 import com.crud.crudClientes.service.ClientService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
@@ -11,5 +15,11 @@ public class ClientController {
 
     public ClientController(ClientService clientService) {
         this.clientService = clientService;
+    }
+
+    @GetMapping
+    public List<ClientDTO> getAllClients() {
+        List<ClientDTO> clients = clientService.getAllClients();
+        return clients;
     }
 }
